f30e542a16edb9a45ebdfde546b84f11df2bf4c1
Diana Ichmoukhametov <akhtyamovadiana@gmail.com>
2024-04-18 09:24:22 -0400

    Add progress line

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -2,24 +2,52 @@ import { useContext } from 'react';
 import FormContext from '../context/FormContext';
 import content from '../content/content';
 
+// const Stepper = () => {
+//   const { state } = useContext(FormContext);
+//   const { currentStep } = state;
+//   console.log(currentStep)
+
+//   return (
+//     <div>
+//       <ul className='steps-container'>
+//         {content.steps.map((step) => (
+//           <li className={`step ${step <= currentStep  ? 'active-step' : ''}`}
+//             key={step}
+//           >
+//             {step}
+       
+//           </li>
+//         ))}
+//                <div className='step-line'></div>
+              
+//       </ul>
+
+//       <hr />
+//     </div>
+//   );
+// };
+
+// export default Stepper;
+
+
 const Stepper = () => {
   const { state } = useContext(FormContext);
   const { currentStep } = state;
-  console.log(currentStep)
 
   return (
     <div>
-      <div className='steps'>
-        {content.steps.map((step) => (
-          <div
-            key={step}
-            style={{ fontWeight: currentStep === step ? 'bold' : 'normal' }}
-          >
-            {step}
+      <div className='steps-container'>
+        {content.steps.map((step, index) => (
+          <div className='step-line' key={step}>
+            <div className={`step ${step <= currentStep ? 'active-step' : ''}`}>
+              {step}
+            </div>
+            {index < content.steps.length - 1 && (
+              <div className={`progress-line ${step <= currentStep ? 'active-progress-line' : ''}`}></div>
+            )}
           </div>
         ))}
       </div>
-      <div></div>
       <hr />
     </div>
   );
